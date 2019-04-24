78c83181be8f6e72c0b7a31f84ef0ab3dbeb4b7b
陈波 <chenb03@mingyuanyun.com>
2019-04-24 13:42:48 +0800

    将abp源代码放在工程便于调试

---
+++ aspnetboilerplate/src/Abp/Configuration/Startup/IMultiTenancyConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Abp.Collections;
+using Abp.MultiTenancy;
+
+namespace Abp.Configuration.Startup
+{
+    /// <summary>
+    /// Used to configure multi-tenancy.
+    /// </summary>
+    public interface IMultiTenancyConfig
+    {
+        /// <summary>
+        /// Is multi-tenancy enabled?
+        /// Default value: false.
+        /// </summary>
+        bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Ignore feature check for host users
+        /// Default value: false.
+        /// </summary>
+        bool IgnoreFeatureCheckForHostUsers { get; set; }
+
+        /// <summary>
+        /// A list of contributors for tenant resolve process.
+        /// </summary>
+        ITypeList<ITenantResolveContributor> Resolvers { get; }
+    }
+}
---
+++ aspnetboilerplate/src/Abp/Configuration/Startup/MultiTenancyConfig.cs
@@ -0,0 +1,30 @@
+﻿using Abp.Collections;
+using Abp.MultiTenancy;
+
+namespace Abp.Configuration.Startup
+{
+    /// <summary>
+    /// Used to configure multi-tenancy.
+    /// </summary>
+    internal class MultiTenancyConfig : IMultiTenancyConfig
+    {
+        /// <summary>
+        /// Is multi-tenancy enabled?
+        /// Default value: false.
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Ignore feature check for host users
+        /// Default value: false.
+        /// </summary>
+        public bool IgnoreFeatureCheckForHostUsers { get; set; }
+
+        public ITypeList<ITenantResolveContributor> Resolvers { get; }
+
+        public MultiTenancyConfig()
+        {
+            Resolvers = new TypeList<ITenantResolveContributor>();
+        }
+    }
+}
