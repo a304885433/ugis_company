9e24dab9cf99c2f570e4c43b44b2e755d9757f41
陈波 <chenb03@mingyuanyun.com>
2019-04-30 00:22:19 +0800

    完成 企业信息 排查点位和字典排序Id， 排查信息的列表和表单

--- aspnet-core/src/MyERP.Application/UGIS/DicTypeAppService.cs
+++ aspnet-core/src/MyERP.Application/UGIS/DicTypeAppService.cs
@@ -1,8 +1,10 @@
 ﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
 using Abp.Domain.Repositories;
 using MyERP.UGIS.Dto;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyERP.UGIS
@@ -10,7 +12,7 @@ namespace MyERP.UGIS
     /// <summary>
     /// 字典类型应用服务
     /// </summary>
-    public class DicTypeAppService : AsyncCrudAppService<DicType, DicTypeDto>
+    public class DicTypeAppService : AsyncCrudAppService<DicType, DicTypeDto, int, GetAllDicTypeInput>
     {
         public DicTypeAppService(IRepository<DicType, int> repository) : base(repository)
         {
--- aspnet-core/src/MyERP.Application/UGIS/Dto/DicDto.cs
+++ aspnet-core/src/MyERP.Application/UGIS/Dto/DicDto.cs
@@ -7,7 +7,31 @@ using System.Text;
 namespace MyERP.UGIS.Dto
 {
     [AutoMapTo(typeof(Dic))]
-    public class DicDto : Dic, IEntityDto
+    public class DicDto : IEntityDto
     {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 父级Id
+        /// </summary>
+        public int ParentId { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 字典类型编码
+        /// </summary>
+        public string TypeCode { get; set; }
+
+        /// <summary>
+        /// 排序Id
+        /// </summary>
+        public int OrderId { get; set; }
     }
 }
