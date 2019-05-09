698711127c8554ab3dda4942a4f49735d13787cf
“冲动” <304885433@qq.com>
2019-05-09 22:31:24 +0800

    增加docker-compose

--- ssh/run.sh
+++
@@ -1,8 +0,0 @@
-# stop and remove all containers
-currcontainer=$(docker ps -a |  grep "$(proj.front)_$(Build.SourceBranchName)*"  | awk '{print $1}')
-if [ "${currcontainer}" != "" ]; then
-   docker stop $(docker ps -a |  grep "$(proj.front)_$(Build.SourceBranchName)*"  | awk '{print $1}')
-   docker rm -f $(docker ps -a |  grep "$(proj.front)_$(Build.SourceBranchName)*"  | awk '{print $1}')
-fi
-# run curr
-docker run -d --volume /home/docker_data:/var/userdata -p $(site.port):80 --name=$(proj.front)_$(Build.SourceBranchName) $(docker.repository):$(proj.front)_$(Build.BuildId)
