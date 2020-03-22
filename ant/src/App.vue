5b0247712c78b288f6fd578ec58612642d7bd1d5
“冲动” <304885433@qq.com>
2020-03-22 21:52:14 +0800

    发布

--- ant/src/App.vue
+++ ant/src/App.vue
@@ -1,34 +1,37 @@
 <template>
   <a-locale-provider :locale="locale">
     <div id="app">
-      <router-view :key="key"/>
+      <router-view :key="key" />
     </div>
   </a-locale-provider>
 </template>
 
 <script>
-import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
-import { AppDeviceEnquire } from '@/utils/mixin'
+  import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+  import { AppDeviceEnquire } from '@/utils/mixin'
 
-export default {
-  mixins: [AppDeviceEnquire],
-  data () {
-    return {
-      locale: zhCN
-    }
-  },
-  computed: {
-    key() {
-      return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route.path
-    }
-  },
-  mounted () {
+  export default {
+    mixins: [AppDeviceEnquire],
+    data() {
+      return {
+        locale: zhCN
+      }
+    },
+    computed: {
+      key() {
+        let d = new Date()
+        let time = d.getMonth() + '_' + d.getDay() + '_' + d.getHours() + '_' + d.getSeconds()
+        return (this.$route.name || this.$route.path) + time
+      }
+    },
+    mounted() {
 
+    }
   }
-}
 </script>
 <style>
   #app {
     height: 100%;
   }
+
 </style>
